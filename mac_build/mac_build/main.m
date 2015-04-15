2ec07cf306242c88a49dacb363437fa868894172
Stanislav Ponomarenko <stas.ponomarenko@hiq.ru>
2015-04-15 16:24:56 +0300

    initial commit

---
+++ mac_build/mac_build/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mac_build
+//
+//  Created by Stanislav Ponomarenko on 15/04/15.
+//  Copyright (c) 2015 Stanislav Ponomarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
