377c68c33e2a5aa26915fe1d244d8ab85be69fcc
Stanislav Ponomarenko <stas.ponomarenko@hiq.ru>
2015-04-15 16:20:24 +0300

    first commit

---
+++ spankki/spankki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  spankki
+//
+//  Created by Stanislav Ponomarenko on 15/04/15.
+//  Copyright (c) 2015 Stanislav Ponomarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
